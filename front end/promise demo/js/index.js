1f20d88a69fb9305fe64f1b34e039da557e400f0
bardrabbit <bardrabbit@gmail.com>
2021-06-02 21:45:55 -0400

    get some data from api

---
+++ front end/promise demo/js/index.js
@@ -0,0 +1,11 @@
+function getImage() {
+  let key = 'wSsCKCmLzbvbCXmn-N5lfAyfX0TzZPHF1JMRhaKPDDU';
+  let uri = 'https://api.unsplash.com/photos/random/?client_id=';
+
+  //   fetch(uri + key).then((res) => console.log(res));
+  fetch(uri + key) // <- promise object
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
